f192ed7a448bcb1520499806f4462f1ba1bfd52f
javiburn <jsarabia98@gmail.com>
2023-09-29 12:59:06 +0200

    new raycasting

--- src/game/raycasting.c
+++ src/game/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 18:42:16 by jsarabia          #+#    #+#             */
-/*   Updated: 2023/09/28 16:45:33 by jsarabia         ###   ########.fr       */
+/*   Updated: 2023/09/29 12:58:44 by jsarabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,114 +14,56 @@
 
 void	check_horizontal_lines(t_game *game)
 {
-	float	y;
-	float	x;
-	int		auxx;
-	int		auxy;
+	float	auxx;
+	float	auxy;
+	int		x;
+	int		y;
 	int		hit;
 
 	hit = 0;
-	y = game->player.y / WALL_SIZE;
-	auxy = y;
-	x = game->player.x / WALL_SIZE;
-	auxx = x;
-	printf("starts at [%d][%d]\n", auxy, auxx);
-	if (game->player.direction > PI)
+	auxx = game->player.x;
+	auxy = game->player.y;
+	x = auxx;
+	y = auxy;
+	printf("pos: %f,%f\n", auxx, auxy);
+	printf("angle: %f\n", game->player.direction);
+	if (game->camera.direction < PI) // looking up
 	{
 		while (hit != 1)
 		{
-			auxx = x;
-			if (auxy < 0 || auxx < 0)
+			if (game->map_data.map[y / WALL_SIZE][x / WALL_SIZE] == '1')
 				hit = 1;
-			else if (auxy < ft_get_matrix_size(game->map_data.map)
-				&& auxx < (int)ft_strlen(game->map_data.map[auxy])
-				&& game->map_data.map[auxy][auxx] == '1')
-				hit = 1;
-			else if (auxy >= ft_get_matrix_size(game->map_data.map)
-				|| auxx >= (float)ft_strlen(game->map_data.map[auxy]))
-				hit = 1;
-			auxy++;
-			x += game->camera.dx / WALL_SIZE;
+			y--;
+			auxx += (game->player.y / y) / tan(game->camera.direction);
+			game->player.y = y;
 		}
 	}
-	if (game->player.direction > 0 && game->player.direction < PI)
+	else if (tan(game->camera.direction) != 0)							// looking down
 	{
 		while (hit != 1)
 		{
-			auxx = x;
-			if (auxy < 0 || auxx < 0)
+			printf("%f\n", tan(PI));
+			if (game->map_data.map[y / WALL_SIZE][x / WALL_SIZE] == '1')
 				hit = 1;
-			else if (auxy < ft_get_matrix_size(game->map_data.map)
-				&& auxx < (int)ft_strlen(game->map_data.map[auxy])
-				&& game->map_data.map[auxy][auxx] == '1')
-				hit = 1;
-			else if (auxy >= ft_get_matrix_size(game->map_data.map)
-				|| auxx >= (float)ft_strlen(game->map_data.map[auxy]))
-				hit = 1;
-			auxy--;
-			x += game->camera.dx / WALL_SIZE;
+			y++;
+			auxx += (y / game->player.y) / tan(game->camera.direction);
+			game->player.y = y;
 		}
 	}
-	printf("wall at [%d][%d]\n", auxy, auxx);
-	game->camera.horizontal = pow((game->player.x / WALL_SIZE) * WALL_SIZE - (x * WALL_SIZE), 2)
-		+ pow((game->player.y / WALL_SIZE) * WALL_SIZE - (auxy * WALL_SIZE), 2);
-	game->camera.horizontal = sqrt(game->camera.horizontal);
 }
 
 void	check_vertical_lines(t_game *game)
 {
-	float	y;
-	float	x;
-	int		auxx;
-	int		auxy;
-	int		hit;
+	float	auxx;
+	float	auxy;
+	int		x;
+	int		y;
 
-	hit = 0;
-	y = game->player.y / WALL_SIZE;
-	auxy = y;
-	x = game->player.x / WALL_SIZE;
-	auxx = x;
-	printf("starts at [%d][%d]\n", auxy, auxx);
-	if (game->player.direction > PI  / 2 && game->player.direction < 3 * PI  / 2)
-	{
-		while (hit != 1)
-		{
-			auxy = y;
-			if (auxy < 0 || auxx < 0)
-				hit = 1;
-			else if (auxy < ft_get_matrix_size(game->map_data.map)
-				&& auxx < (int)ft_strlen(game->map_data.map[auxy])
-				&& game->map_data.map[auxy][auxx] == '1')
-				hit = 1;
-			else if (auxy >= ft_get_matrix_size(game->map_data.map)
-				|| auxx >= (float)ft_strlen(game->map_data.map[auxy]))
-				hit = 1;
-			auxx++;
-			y += game->camera.dy / WALL_SIZE;
-		}
-	}
-	else
-	{
-		while (hit != 1)
-		{
-			auxy = y;
-			if (auxy < 0 || auxx < 0)
-				hit = 1;
-			else if (auxy < ft_get_matrix_size(game->map_data.map)
-				&& auxx < (int)ft_strlen(game->map_data.map[auxy])
-				&& game->map_data.map[auxy][auxx] == '1')
-				hit = 1;
-			else if (auxy >= ft_get_matrix_size(game->map_data.map)
-				|| auxx >= (float)ft_strlen(game->map_data.map[auxy]))
-				hit = 1;
-			auxx--;
-			y += game->camera.dy / WALL_SIZE;
-		}
-	}
-	printf("wall at [%d][%d]\n", auxy, auxx);
-	game->camera.vertical = pow((game->player.x / WALL_SIZE) * WALL_SIZE - (auxx * WALL_SIZE), 2)
-		+ pow((game->player.y / WALL_SIZE) * WALL_SIZE - (y * WALL_SIZE), 2);
-	game->camera.vertical = sqrt(game->camera.vertical);
+	auxx = game->player.x;
+	auxy = game->player.y;
+	x = auxx;
+	y = auxy;
+	printf("pos: %d,%d\n", x, y);
 }
 
 void	raycasting(t_game *game)
@@ -149,7 +91,7 @@ void	raycasting(t_game *game)
 			game->camera.distance = game->camera.horizontal;
 			game->camera.offset = 0;
 		}
-		ft_draw_wall(game);
+		//ft_draw_wall(game);
 		game->camera.direction -= 0.00054541539;
 		game->camera.fov += 0.03125;
 	}
