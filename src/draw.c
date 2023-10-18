c50c7849006004cc15bf2a1aae0ed30c27fb0b5d
magnitopic <magnitopic@gmail.com>
2023-10-18 13:07:03 +0200

    fixed ft_put_pixel

--- src/draw.c
+++ src/draw.c
@@ -6,18 +6,15 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 10:01:04 by alaparic          #+#    #+#             */
-/*   Updated: 2023/10/16 15:47:02 by alaparic         ###   ########.fr       */
+/*   Updated: 2023/10/18 13:04:15 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
 
 // ! Bugged function
-void	ft_put_pixel(t_img img, int x, int y, t_color rgb)
+void	ft_put_pixel(t_img img, int x, int y, int color)
 {
-	int		color;
-
-	color = ft_rgba(rgb.red, rgb.green, rgb.blue, 0);
 	if (x >= 0 && y >= 0 && x < SCREEN_WIDTH && y < SCREEN_HEIGHT)
 		*(int *)&img.addr[((x * img.bpp) >> 3) + (y * img.line_len)] = color;
 }
@@ -76,7 +73,8 @@ void	ft_draw_wall(t_game *game)
 			else
 				texcolor = game->textu_e.text_value[(int)(WALL_SIZE * game->camera.texty + game->camera.textx)];
 		}
-		mlx_pixel_put(game->mlx, game->win, x, start, texcolor);
+		//mlx_pixel_put(game->mlx, game->win, x, start, texcolor);
+		ft_put_pixel(game->img, x, start, texcolor);
 		// TODO: Add shadows to side
 		start++;
 	}
@@ -95,18 +93,11 @@ void	draw_ceiling_floor(t_game *game, t_color ceiling, t_color floor)
 		while (x++ < SCREEN_WIDTH)
 		{
 			if (y < SCREEN_HEIGHT / 2)
-			{
-				//ft_put_pixel(game->img, x, y, floor);
-				mlx_pixel_put(game->mlx, game->win, x, y, ft_rgba(ceiling.red, ceiling.green, ceiling.blue, 0));
-			}
+				ft_put_pixel(game->img, x, y, ft_rgba(floor.red, floor.green, floor.blue, 0));
 			else
-			{
-				//ft_put_pixel(game->img, x, y, ceiling);
-				mlx_pixel_put(game->mlx, game->win, x, y, ft_rgba(floor.red, floor.green, floor.blue, 0));
-			}
+				ft_put_pixel(game->img, x, y, ft_rgba(ceiling.red, ceiling.green, ceiling.blue, 0));
 		}
 	}
-	//mlx_put_image_to_window(game->mlx, game->win, game->img.img, 0, 0);
 }
 
 void	re_draw_screen(t_game *game)
@@ -114,4 +105,5 @@ void	re_draw_screen(t_game *game)
 	draw_ceiling_floor(game, game->map_data.ceiling, game->map_data.floor);
 	//minimap(game, game->map_data.map);
 	raycasting(game);
+	mlx_put_image_to_window(game->mlx, game->win, game->img.img, 0, 0);
 }
