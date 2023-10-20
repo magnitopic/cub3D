f13be772084b008eb9f576a996a610ad89a07e72
magnitopic <magnitopic@gmail.com>
2023-10-20 12:33:45 +0200

    fixed event handler for checking walls

--- src/event_handler.c
+++ src/event_handler.c
@@ -6,21 +6,20 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 14:50:34 by alaparic          #+#    #+#             */
-/*   Updated: 2023/10/18 19:23:40 by alaparic         ###   ########.fr       */
+/*   Updated: 2023/10/20 12:25:43 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
 
-static int	check_for_walls(t_game *game, double x, double y)
+static int	check_for_walls(t_game *game)
 {
-	if (x <= WALL_SIZE || (int)x / WALL_SIZE
+	if (game->player.old_y <= WALL_SIZE || (int)game->player.old_x / WALL_SIZE
 		>= ft_get_matrix_size(game->map))
 		return (0);
-	if (y <= WALL_SIZE || (int)y / WALL_SIZE
-		>= (int)ft_strlen(game->map[(int)x / WALL_SIZE]))
-		return (0);
-	if (game->map[(int)x / WALL_SIZE][(int)y / WALL_SIZE] != '1')
+	if (game->player.old_x <= WALL_SIZE || (int)game->player.old_y
+		/ WALL_SIZE >= (int)ft_strlen(game->map
+			[(int)game->player.old_x / WALL_SIZE]))
 		return (0);
 	return (1);
 }
@@ -52,26 +51,26 @@ static void	move_back_and_forth(t_game *game, enum e_keys key)
 	{
 		game->player.old_x = game->player.x + game->player.dir.x * SPEED;
 		game->player.old_y = game->player.y + game->player.dir.y * SPEED;
-		check_for_walls(game, game->player.old_x, game->player.old_y);
+		if (!check_for_walls(game))
+			return ;
 		if (game->map[(int)game->player.old_x / WALL_SIZE]
 			[(int)game->player.y / WALL_SIZE] != '1')
 			game->player.x += game->player.dir.x * SPEED;
 		if (game->map[(int)game->player.x / WALL_SIZE]
 			[(int)game->player.old_y / WALL_SIZE] != '1')
 			game->player.y += game->player.dir.y * SPEED;
+		return ;
 	}
-	else if (key == S || key == DOWN)
-	{
-		game->player.old_x = game->player.x - game->player.dir.x * SPEED;
-		game->player.old_y = game->player.y - game->player.dir.y * SPEED;
-		check_for_walls(game, game->player.old_x, game->player.old_y);
-		if (game->map[(int)game->player.old_x / WALL_SIZE]
-			[(int)game->player.y / WALL_SIZE] != '1')
-			game->player.x -= game->player.dir.x * SPEED;
-		if (game->map[(int)game->player.x / WALL_SIZE]
-			[(int)game->player.old_y / WALL_SIZE] != '1')
-			game->player.y -= game->player.dir.y * SPEED;
-	}
+	game->player.old_x = game->player.x - game->player.dir.x * SPEED;
+	game->player.old_y = game->player.y - game->player.dir.y * SPEED;
+	if (!check_for_walls(game))
+		return ;
+	if (game->map[(int)game->player.old_x / WALL_SIZE]
+		[(int)game->player.y / WALL_SIZE] != '1')
+		game->player.x -= game->player.dir.x * SPEED;
+	if (game->map[(int)game->player.x / WALL_SIZE]
+		[(int)game->player.old_y / WALL_SIZE] != '1')
+		game->player.y -= game->player.dir.y * SPEED;
 }
 
 static void	move_side_to_side(t_game *game, enum e_keys key)
@@ -80,26 +79,26 @@ static void	move_side_to_side(t_game *game, enum e_keys key)
 	{
 		game->player.old_x = game->player.x - game->player.dir.y * SPEED;
 		game->player.old_y = game->player.y + game->player.dir.x * SPEED;
-		check_for_walls(game, game->player.old_x, game->player.old_y);
+		if (!check_for_walls(game))
+			return ;
 		if (game->map[(int)game->player.old_x / WALL_SIZE]
 			[(int)game->player.y / WALL_SIZE] != '1')
 			game->player.x -= game->player.dir.y * SPEED;
 		if (game->map[(int)game->player.x / WALL_SIZE]
 			[(int)game->player.old_y / WALL_SIZE] != '1')
 			game->player.y += game->player.dir.x * SPEED;
+		return ;
 	}
-	else if (key == D)
-	{
-		game->player.old_x = game->player.x + game->player.dir.y * SPEED;
-		game->player.old_y = game->player.y - game->player.dir.x * SPEED;
-		check_for_walls(game, game->player.old_x, game->player.old_y);
-		if (game->map[(int)game->player.old_x / WALL_SIZE]
-			[(int)game->player.y / WALL_SIZE] != '1')
-			game->player.x += game->player.dir.y * SPEED;
-		if (game->map[(int)game->player.x / WALL_SIZE]
-			[(int)game->player.old_y / WALL_SIZE] != '1')
-			game->player.y -= game->player.dir.x * SPEED;
-	}
+	game->player.old_x = game->player.x + game->player.dir.y * SPEED;
+	game->player.old_y = game->player.y - game->player.dir.x * SPEED;
+	if (!check_for_walls(game))
+		return ;
+	if (game->map[(int)game->player.old_x / WALL_SIZE]
+		[(int)game->player.y / WALL_SIZE] != '1')
+		game->player.x += game->player.dir.y * SPEED;
+	if (game->map[(int)game->player.x / WALL_SIZE]
+		[(int)game->player.old_y / WALL_SIZE] != '1')
+		game->player.y -= game->player.dir.x * SPEED;
 }
 
 int	event_handler(enum e_keys key, t_game *game)
